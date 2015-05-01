48935db6b1c6d6b3b58b551a64c3dba98cdcf86f
Craig T <ecrfat@warwick.ac.uk>
2015-05-01 15:55:31 +0100

    Add helper method to get number of observations from PROC SQL.
    Setup to work with delimiter outputted in log file from PROC SQL.

--- pywrds/raw_queries.py
+++ pywrds/raw_queries.py
@@ -9,6 +9,11 @@ class BaseQuery(object):
         self.query = query
         self.file_name = query_name
         self._local_path = ''
+        # Set the output of the query to be a .tsv file and store the file
+        # trunk.
+        self.trunk = query_name.split('.')[0]
+        self.out_filename = self.trunk + '.tsv'
+        self.log_file = self.trunk + '.log'
 
     def _write2local(self, local_path):
         """Write query to local_path with self.file_name and store its location.
@@ -22,11 +27,35 @@ class BaseQuery(object):
         self._local_path = local_path
         return
 
-    def _remove_file_from_local(self):
+    def _remove_from_local(self):
         """Remove the file from the local path.
         """
         # self._local_path, self.file_name
-        return NotImplementedError
+        return True
+
+    @staticmethod
+    def get_nobs(session, log_filename, delimiter=None):
+        """When creating a table with sql, the n obs is in this format:
+            "NOTE: Table OUT.CRSP_RETS created, with 70536 rows and 6 columns."
+        So, for time being just take the first number following delimiter
+        'created, with'
+
+        :param session (WrdsSession):
+        :param log_filename:
+        :param delimiter: String after which the first number is returned.
+        :return:
+        """
+        # Default delimiter for PROC SQL.
+        if not delimiter:
+            delimiter = 'created, with'
+
+        # Recover the first digits after the delimiter.
+        cmd = "perl -nle 'print $1 if /{0}.*?(\d+)/' {1}".format(delimiter,
+                                                                 log_filename)
+
+        [stdin, stdout, stderr] = session.ssh.exec_command(cmd)
+        n_obs_raw = (stdout.readlines())
+        return n_obs_raw[0].strip()
 
     @staticmethod
     def ensure_dir(path):
@@ -58,7 +87,7 @@ def eg_query_crsp():
     query = """
     /* Year Range */
     %LET FIRST = 1965;
-    %LET LAST = 1968;
+    %LET LAST = 1967;
     %LET crsp_rets = a.PERMNO, a.DATE, b.SICCD, b.EXCHCD, b.SHRCD, a.RET;
         proc sql;
         create table out.crsp_rets as
@@ -74,4 +103,23 @@ def eg_query_crsp():
         """
     return BaseQuery(query, 'test_crsp.sas')
 
+
+def eg_nobs_crsp():
+    query = """
+    proc sql noprint;
+        select count(*) into : nobs
+        from out.crsp_rets;
+    quit;
+    %put 'Obs in data set:' &nobs;
+    """
+    alt_query = """
+    proc sql noprint;
+        select nlobs into : nobs
+        from dictionary.tables
+        where libname='out'
+        and memname='crsp_rets';
+    quit;
+    %put 'Obs in data set:' &nobs;
+    """
+    return BaseQuery(query, 'n_crsp.sas')
 
